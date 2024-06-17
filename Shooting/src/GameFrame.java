b125d8b6f5f23719bfd7590bafbfba3474c6dced
sigaharu <zeal23310007@fsgcl.jp>
2024-06-17 16:54:49 +0900

    10-7

--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -6,21 +6,25 @@ public class GameFrame extends MyFrame{
         addKeyListener(GameWorld.player);
         GameWorld.playerBullets=new Vector<PlayerBullet>();
         while(true) {
-            clear();
+        	clear();
             GameWorld.player.draw(this);
             GameWorld.player.move();
-            int i=0;
-            while (i<GameWorld.playerBullets.size()) {
-                PlayerBullet b=GameWorld.playerBullets.get(i);
-                b.draw(this);
-                b.move();
-                if(b.y<0) {
-                	GameWorld.playerBullets.remove(i);
-                }else {
-                	i++;
-                }
-            }
+            movePlayerBullets();
             sleep(0.03);
+        }
+    }
+ 
+    private void movePlayerBullets() {
+        int i=0;
+        while (i<GameWorld.playerBullets.size()) {
+            PlayerBullet b=GameWorld.playerBullets.get(i);
+            b.draw(this);
+            b.move();
+            if(b.y<0) {
+                GameWorld.playerBullets.remove(i);
+            } else {
+                i++;
+            }
         }
     }
 }

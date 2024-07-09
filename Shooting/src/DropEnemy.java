d604d1f9f788438dcc08de0c3a78f1157899da76
sigaharu <zeal23310007@fsgcl.jp>
2024-07-09 10:38:07 +0900

    練習問題11-2

--- Shooting/src/DropEnemy.java
+++ Shooting/src/DropEnemy.java
@@ -1,7 +1,13 @@
 
 public class DropEnemy extends Enemy{
-	public DropEnemy(double x,double y,double vx,double vy) {
-		super(x,y,vx,vy);
+	public  void draw(MyFrame f) {
+		f.setColor(0, 128, 0);
+		f.fillRect(x,y,30,10);
+		f.setColor(0, 128, 0);
+		f.fillRect(x+10,y,10,30);
+	}
+	public DropEnemy(double x, double y, double vx, double vy) {
+		super(x, y, vx, vy);
 	}
 	public void move() {
 		super.move();

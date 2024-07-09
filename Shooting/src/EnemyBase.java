14eca80faad495200fec50ce00743e06f05ffb68
sigaharu <zeal23310007@fsgcl.jp>
2024-07-09 10:35:26 +0900

    練習問題11-1

--- Shooting/src/EnemyBase.java
+++ Shooting/src/EnemyBase.java
@@ -1,15 +1,15 @@
 
 public class EnemyBase extends Enemy{
-	public void draw(MyFrame f) {
+	public  void draw(MyFrame f) {
 		f.setColor(0, 128, 0);
-		f.fillOval(x, y, 32 ,32);
+		f.fillOval(x,y,32,32);
 		f.setColor(200, 200, 200);
-		f.fillOval(x-16, y+8, 64 ,16);
+		f.fillOval(x-16,y+8,64,16);
 	}
-	public EnemyBase(double x, double y,double vx,double vy) {
-		super(x,y,vx,vy);
+	public EnemyBase(double x, double y, double vx, double vy) {
+		super(x, y, vx, vy);
 	}
-	public void move(){
+	public void move() {
 		super.move();
 		if(x>300) vx=-1;
 		if(x<100) vx=1;
@@ -18,6 +18,12 @@ public class EnemyBase extends Enemy{
 		}
 		if(Math.random()<0.05) {
 			GameWorld.enemies.add(new RandomEnemy(x,y,0,1));
+		}
+		if(Math.random()<0.05) {
+			GameWorld.enemies.add(new DropEnemy(x,y,0,1));
+		}
+		if(Math.random()<0.05) {
+			GameWorld.enemies.add(new CurveEnemy(x,y,0,2));
 		}
 	}
 }
